7baa375097bd25f1b518ea29634532b8cb17ff4e
wuhao <576677381@qq.com>
2016-06-03 21:20:06 +0800

    article

---
+++ article/models.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+from django.db import models
+from django.contrib.auth.models import User
+from block.models import Blocks
+# Create your models here.
+
+class Article(object):
+    title=models.CharField(u"标题",max_length=30)
+    content=models.CharField(u"内容",max_length=10000)
+    status=models.CharField(u"状态",choices=((0,u"普通"),(-1,u"删除"),(10,"精华")),default=0)
+    owner=models.ForeignKey(User,verbose_name=u"作者")
+    block=models.ForeignKey(Blocks,verbose_name=u"板块")
+
+    create_timestamp=models.DateField(u"创建时间",auto_now_add=True)
+    last_update_timestamp=models.DateField(u"更新时间",auto_now=True)
+
+    class Meta:
+        verbose_name=u"文章"
+        verbose_name_plural=u"文章"
