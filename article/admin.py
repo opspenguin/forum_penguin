dda69683f6f2c5c5e2bc5c7cc31bd223736ff8f5
ops_penguin <wuhao@zlongame.com>
2016-06-08 11:30:58 +0800

    add articleadmin

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from models import Article
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title","status","owner","block","create_timestamp","last_update_timestamp")
+    search_fields = ["title","status","content"]
+    list_filter = ("block",)
+
+admin.register(Article,ArticleAdmin)
